2510ce1c3dcaa0ae0f2b67b27d0490d746bd8b6b
Mohamed Sobh <malisobh2010@gmail.com>
2025-04-12 10:15:31 +0200

    test

--- WebSecService/app/Http/Controllers/Web/ProductsController.php
+++ WebSecService/app/Http/Controllers/Web/ProductsController.php
@@ -15,6 +15,7 @@ class ProductsController extends Controller {
 	public function __construct()
     {
         $this->middleware('auth:web')->except('list');
+        /**/	
     }
 
 	public function list(Request $request) {
