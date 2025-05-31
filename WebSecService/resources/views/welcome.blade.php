5459e80db1f957cbfa45c92c02e2289156023e2e
Mohamed Sobh <malisobh2010@gmail.com>
2025-05-31 08:16:00 +0300

    Add Version

--- WebSecService/resources/views/welcome.blade.php
+++ WebSecService/resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="card m-4">
       <div class="card-body" style="color:red;font-weight: bolder;">
-        Welcome to Home Page (Final Exam Version - Spring 2025)
+        Welcome to Home Page (Final Exam Version - Spring 2025) - Version 1.01
       </div>
     </div>
 @endsection
